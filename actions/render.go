5633460b027afdc984dd79eb8efcc672709691be
nickolas@knicklabs.com <nickolas@knicklabs.com>
2017-04-13 22:46:55 -0400

    Remove template helpers
    No longer render templates server side

--- actions/render.go
+++ actions/render.go
@@ -19,28 +19,6 @@ func init() {
 		TemplatesBox: packr.NewBox("../templates"),
 
 		// Add template helpers here:
-		Helpers: render.Helpers{
-			"errClass": func(hasError bool) string {
-				if hasError {
-					return "has-danger"
-				}
-				return ""
-			},
-			"errMsg": func(errors map[string][]string, key string) template.HTML {
-				if value, ok := errors[key]; ok {
-					element  := strings.Join(value, "<br>")
-					elements := []string{"<span class='help-block'>", element, "</span>"}
-					html := strings.Join(elements, "")
-					return template.HTML(html)
-				}
-
-				return template.HTML("")
-			},
-			"helpMsg": func(message string) template.HTML {
-				elements := []string{"<span class='help-block'>", message, "</span>"}
-				html := strings.Join(elements, "")
-				return template.HTML(html)
-			},
-		},
+		Helpers: render.Helpers{},
 	})
 }
